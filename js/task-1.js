bc23a074f5af9bdf4e986167d3855b19c167ce2f
Iryna Konoplova <a.irina773@gmail.com>
2021-03-19 01:37:55 +0200

    Решает задачи 1 - 8

---
+++ js/task-1.js
@@ -0,0 +1,35 @@
+// Функция calculateTotalPrice(orderedItems) принимает один параметр orderedItems - массив чисел,
+// и рассчитывает общую сумму его элементов, которая сохраняется в переменной totalPrice и возвращается
+// как результат работы функции.
+
+// Выполни рефакторинг функции так, чтобы вместо цикла for она использовала метод forEach.
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Пиши код ниже этой строки
+
+//   for (let i = 0; i < orderedItems.length; i += 1) {
+//     totalPrice += orderedItems[i];
+//   }
+
+//   // Пиши код выше этой строки
+//   return totalPrice;
+// }
+
+
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Пиши код ниже этой строки
+
+ 
+  orderedItems.forEach(item => totalPrice +=item)
+  
+  // Пиши код выше этой строки
+  return totalPrice;
+}
+
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
