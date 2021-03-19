7279d40a3dadf48170b414398a8488d7f81902fd
Iryna Konoplova <a.irina773@gmail.com>
2021-03-20 01:28:20 +0200

    Решает задачи 9 - 15

--- js/task-9.js
+++ js/task-9.js
@@ -16,3 +16,22 @@
 //     // Пиши код выше этой строки
 //   }
 
+function changeEven(numbers, value) {
+    // Пиши код ниже этой строки
+    const newArray = [];
+  
+    numbers.forEach(function(number){
+      if (number % 2 === 0) {
+         number = number + value;
+      }
+      newArray.push(number);
+      
+    });
+  return newArray;
+    // Пиши код выше этой строки
+  }
+
+console.log(changeEven([1, 2, 3, 4, 5], 10));
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
+console.log(changeEven([17, 24, 68, 31, 42], 100));
+console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
