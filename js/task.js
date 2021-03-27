ceabf9addce0610aa655ae2c3b0634ca96f4b085
Iryna Konoplova <a.irina773@gmail.com>
2021-03-28 00:26:28 +0200

    Решает дополнительную задачу

--- js/task.js
+++ js/task.js
@@ -15,59 +15,23 @@ const products = [
 
 const getAllPropValues = (array, propName) => {
     
-    // const arrayOfProperties = [];
-
+  const arrayOfProperties = [];
+   
     array.forEach(object => {
-        const arrayKeys = Object.keys(object);
-        console.log(arrayKeys);
-    })
-
-     return  arrayKeys.filter(key => key === propName)
-  console.log(arrayKeys);
-    // for (const object of arr) {
-    //     const arrayKeys = Object.keys(object);
-    //     for (const key of arrayKeys) {
-    //         if (key === prop) {
-    //             arrayOfProperties.push(object[key]);
-    //         }
-    //     }
-    // }
-    // return arrayOfProperties;
-};
-
-console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
-
-console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
-
-console.log(getAllPropValues(products, 'category')); // []
-
-
-
-
-// const products = [
-//   { name: 'Радар', price: 1300, quantity: 4 },
-//   { name: 'Сканер', price: 2700, quantity: 3 },
-//   { name: 'Дроид', price: 400, quantity: 7 },
-//   { name: 'Захват', price: 1200, quantity: 2 },
-// ];
-
-// const getAllPropValues = function (arr, prop) {
+      if (Object.keys(object).includes(propName)) {
+          arrayOfProperties.push(object[propName])
+        }
+    } )
+   
+  return arrayOfProperties;
     
-//     const arrayOfProperties = [];
-  
-//     for (const object of arr) {
-//         const arrayKeys = Object.keys(object);
-//         for (const key of arrayKeys) {
-//             if (key === prop) {
-//                 arrayOfProperties.push(object[key]);
-//             }
-//         }
-//     }
-//     return arrayOfProperties;
-// };
+  }
 
-// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
 
-// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+console.log(getAllPropValues(products, 'name')); 
+console.log(getAllPropValues(products, 'price'));
+console.log(getAllPropValues(products, 'quantity'));
+console.log(getAllPropValues(products, 'category'));
 
-// console.log(getAllPropValues(products, 'category')); // []
+
+console.log(products)
